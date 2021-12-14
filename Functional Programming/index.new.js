834618d67966a4a52c71ded56efd1042c3d1b637
mayuxian <mayuxian@autohome.com.cn>
2021-12-14 15:38:58 +0800

    完善bind和柯里化函数

--- Functional Programming/index.new.js
+++ Functional Programming/index.new.js
@@ -43,3 +43,16 @@ function curry(fn) {
     return fn(...args)
   }
 }
+
+
+//ramda和loadash中柯里化函数
+const curry = (fn, ...args) =>
+  // 函数的参数个数可以直接通过函数数的.length属性来访问
+  args.length >= fn.length // 这个判断很关键！！！
+    // 传入的参数大于等于原始函数fn的参数个数，则直接执行该函数
+    ? fn(...args)
+    /**
+     * 传入的参数小于原始函数fn的参数个数时
+     * 则继续对当前函数进行柯里化，返回一个接受所有参数（当前参数和剩余参数） 的函数
+    */
+    : (..._args) => curry(fn, ...args, ..._args);
--- call & bind & apply/index.js
+++ call & bind & apply/index.js
@@ -23,12 +23,22 @@ Function.prototype._apply = function (context) {
 
 
 Function.prototype._bind = function (context) {
-  let _this = this;
-  let args = [...arguments].slice(1);
-  return function BindFn() {
-    if (this instanceof BindFn) {
-      return new _this(...args, ...arguments);
-    }
-    return _this._apply(context, args.concat(...arguments));
+  // let _this = this;
+  // let args = [...arguments].slice(1);
+  // return function BindFn() {
+  //   if (this instanceof BindFn) {
+  //     return new _this(...args, ...arguments);
+  //   }
+  //   return _this._apply(context, args.concat(...arguments));
+  // }
+  //---------------
+  var self = this;
+  // 获取bind2函数从第二个参数到最后一个参数
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return function () {
+    // 这个时候的arguments是指bind返回的函数传入的参数
+    var bindArgs = Array.prototype.slice.call(arguments);
+    return self.apply(context, args.concat(bindArgs));
   }
 }
