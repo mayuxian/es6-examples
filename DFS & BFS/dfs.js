bc48a4c7460734e1ede3edef7c914a35c9aeaf5f
mayuxian <mayuxian@100doc.com.cn>
2022-01-27 20:31:38 +0800

    更新bfs

--- DFS & BFS/dfs.js
+++ DFS & BFS/dfs.js
@@ -68,13 +68,50 @@ function LDR_Traversal() {
 /**
  * 后序遍历-递归法
  */
-function LRD_Recursion() {
-
+function LRD_Recursion(root) {
+  const res = []
+  const dfs = (root) => {
+    dfs(root.left)
+    dfs(root.right)
+    res.push(root.val)
+  }
+  dfs(root)
+  return res;
 }
 
 /**
  * 后序遍历-遍历法
  */
-function LDR_Traversal() {
-
+function LDR_Traversal(root) {
+  var p = root
+  var stack = [root]
+  var res = []
+  while(stack.length>0){
+      p = stack.pop()
+      res.push(p.val)
+      if (p.left!==null){
+          stack.push(p.left)
+      }
+      if (p.right!==null){
+          stack.push(p.right)
+      }
+  }
+  res.reverse()
+  return res
+}
+function LDR_Traversal_test(root){
+  const res = [];
+  const stack = [];
+  let p = root;
+  while (stack.length) {
+    if (p) {
+      stack.push(p)
+      p = p.left
+    } else {
+      p = stack.pop();
+      if (p.left) res.push(p.left)
+      if (p.right) res.push(p.right)
+      p = p.right
+    }
+  }
 }
